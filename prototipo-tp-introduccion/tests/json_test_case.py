ea6eb3ebfb9d3496c96aa010959dca6f889e6f7c
FrancoBar <fbarreneche@fi.uba.ar>
2026-08-04 18:54:25 -0300

    fix: replace clint with client in json test case

--- prototipo-tp-introduccion/tests/json_test_case.py
+++ prototipo-tp-introduccion/tests/json_test_case.py
@@ -10,7 +10,7 @@ class Json(TestCase):
         server_files = shell_cmd.search_files_containing("./services/server", "import json")
         if len(server_files) > 0:
             raise LookupError(f"json is imported in the server")
-        client_files = shell_cmd.search_files_containing("./services/clint", "encoding/json")
+        client_files = shell_cmd.search_files_containing("./services/client", "encoding/json")
         if len(client_files) > 0:
             raise LookupError(f"json is imported in the client")
 
