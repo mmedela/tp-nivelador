b903869a83742b9bbdd8902bbb8433394b996e90
FrancoBar <fbarreneche@fi.uba.ar>
2026-08-13 20:55:34 -0300

    feat: remove uneeded separate stop process

--- prototipo-tp-introduccion/tests/sigterm_test_case.py
+++ prototipo-tp-introduccion/tests/sigterm_test_case.py
@@ -1,14 +1,7 @@
-import multiprocessing
-
 from .utils import docker, docker_compose
 from .test_case import TestCase
 
 DOCKER_COMPOSE_PATH = "docker-compose-no-agency-quorum.yaml"
-
-
-def _await_target_service(target_service_name, pipe_send) -> None:
-    zero_exit_code_count = docker.await_containers([target_service_name])
-    pipe_send.send(zero_exit_code_count)
 
 
 class SigtermHandling(TestCase):
@@ -18,20 +11,10 @@ class SigtermHandling(TestCase):
     @staticmethod
     def _test_stopping_service(server_service_name, target_service_name) -> None:
         SigtermHandling.await_net_io_stop(server_service_name)
-        pipe_recv, pipe_send = multiprocessing.Pipe()
-        stop_service_process = None
-        try:
-            stop_service_process = multiprocessing.Process(
-                target=_await_target_service, args=(target_service_name, pipe_send)
-            )
-            stop_service_process.start()
-            docker.stop([target_service_name])
-            zero_exit_code_count = pipe_recv.recv()
-            if zero_exit_code_count != 1:
-                raise ValueError(f"{target_service_name} exited with an error code")
-        finally:
-            if stop_service_process:
-                stop_service_process.join()
+        docker.stop([target_service_name])
+        zero_exit_code_count = docker.await_containers([target_service_name])
+        if zero_exit_code_count != 1:
+            raise ValueError(f"{target_service_name} exited with an error code")
 
     @staticmethod
     def test() -> None:
